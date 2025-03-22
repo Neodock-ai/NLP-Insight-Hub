be709604dc3715d760690e4f7a309edb38e3106c
Neodock-ai <devanshkumar1998@gmail.com>
2025-03-21 20:54:35 -0400

    Add files via upload

---
+++ models/init.py
@@ -0,0 +1,6 @@
+from .llama import LlamaModel
+from .falcon import FalconModel
+from .mistral import MistralModel
+from .deepseek import DeepSeekModel
+
+__all__ = ['LlamaModel', 'FalconModel', 'MistralModel', 'DeepSeekModel']
