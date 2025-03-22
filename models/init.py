1e5078ffe7fd8b7ab17c1c640eebbeaf59f1b790
Neodock-ai <devanshkumar1998@gmail.com>
2025-03-21 20:50:48 -0400

    Delete models directory

--- models/init.py
+++
@@ -1,6 +0,0 @@
-from .llama import LlamaModel
-from .falcon import FalconModel
-from .mistral import MistralModel
-from .deepseek import DeepSeekModel
-
-__all__ = ['LlamaModel', 'FalconModel', 'MistralModel', 'DeepSeekModel']
